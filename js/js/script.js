4328f6a4f61dc643baf8011cb9c1084b12a16832
EmmanuelHdzC <menny_hrz@hotmail.com>
2020-10-31 21:27:42 +0100

    Cuento Infantil

---
+++ js/js/script.js
@@ -0,0 +1,12 @@
+console.log(5);
+console.log("Hola mundo!".length);
+console.log(234.456);
+console.log(2+3);
+console.log(34-12);
+
+var x = 10;
+x = 'Hola';
+let y = 15;
+const vacio = false;
+
+console.log("Hola Mundo".search("la"));
